c9262276c1f4cd39b8f2773279fd4e0a66d6dc68
Luc Ho <hotriluc97@gmail.com>
2022-10-11 12:40:02 +0200

    complete detail card

--- src/components/pages/CharacterDetail.tsx
+++ src/components/pages/CharacterDetail.tsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ICharacter } from '../../interfaces/Character.interface';
+import classes from './CharacterDetail.module.css';
 
 const CharacterDetail = (): JSX.Element => {
   const params = useParams();
@@ -22,7 +23,65 @@ const CharacterDetail = (): JSX.Element => {
     fetchCharacterDetails();
   }, [id]);
 
-  return <div>{character?._id}</div>;
+  let films = <p>No Films</p>;
+  let tvShows = <p>No TV Shows</p>;
+  let videoGames = <p>No Video games</p>;
+
+  if (character?.films.length) {
+    films = (
+      <ul>
+        {character.films.map((film, id) => (
+          <li key={id}>{film}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  if (character?.tvShows.length) {
+    tvShows = (
+      <ul>
+        {character.tvShows.map((show, id) => (
+          <li key={id}>{show}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  if (character?.videoGames.length) {
+    videoGames = (
+      <ul>
+        {character.videoGames.map((game, id) => (
+          <li key={id}>{game}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <div className={classes.character}>
+      <div className={classes['info']}>
+        <h1>{character?.name || ''}</h1>
+        <div className={classes['img-holder']}>
+          <img src={character?.imageUrl} className={classes['img']} alt="" />
+        </div>
+      </div>
+
+      <div className={classes['references']}>
+        <div>
+          <h2>Films</h2>
+          {films}
+        </div>
+        <div>
+          <h2>TV Shows</h2>
+          {tvShows}
+        </div>
+        <div>
+          <h2>Video games</h2>
+          {videoGames}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CharacterDetail;
--- src/interfaces/Character.interface.ts
+++ src/interfaces/Character.interface.ts
@@ -2,6 +2,7 @@ export interface ICharacter {
   _id: number;
   name: string;
   imageUrl: string;
+  films: Array<string>;
   videoGames: Array<string>;
   tvShows: Array<string>;
   shortFilms: Array<string>;
